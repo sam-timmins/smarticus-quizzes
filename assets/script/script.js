97446d03f721b7c6942504cc36e906d9ef33a109
sam-timmins <sam.timmins1@gmail.com>
2021-08-04 19:57:21 +0000

    Change organisation of functions for better readability in script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,33 @@
+// Hide user form
+function hideUserForm(){
+    let hideUserForm = document.querySelector('#user');
+    hideUserForm.style.display = 'none';
+}
+
+// Hide catagories grid
+function hideCatagoriesGrid(){
+    let hide = document.querySelector('#main-catagory-grid');
+    hide.style.display='none';
+}
+
+// Show catagories grid 
+function showCatagoriesGrid(){
+    let show = document.querySelector('#main-catagory-grid');
+    show.style.display = 'grid';
+}
+
+// Show questions section
+function openQuestionScreen(){
+    let openScreen = document.querySelector('#questions');
+    openScreen.style.display = 'flex';
+}
+
+// Close questions sction
+function closeQuestionScreen(){
+    let close = document.querySelector('#questions');
+    close.style.display = 'none';
+}
+
 /**
  * Logs the name of the user from user-form and then closes the user-form, 
  * adds name to the welcome box and opens catagory grid.
@@ -8,24 +38,16 @@ userForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
     event.preventDefault();
-
-
+    
     let user = document.querySelector('#user-name');
     let welcomeDiv = document.querySelector('#welcome-person');
     let welcomeMesage = document.createElement('p');
     welcomeMesage.innerHTML = 'Hi ' + user.value + '!';
     welcomeDiv.insertBefore(welcomeMesage, welcomeDiv.childNodes[0]);
-
-
-    // Hide user form
-    let hideUserForm = document.querySelector('#user');
-    hideUserForm.style.display = 'none';
-
-    // Show catagories grid 
-    let showCatagoriesGrid = document.querySelector('#main-catagory-grid');
-    showCatagoriesGrid.style.display = 'grid';
+    
+    hideUserForm();
+    showCatagoriesGrid();
 }
-
 
 /**
  * Opens sports question and answer box and closes the catagories grid
@@ -35,24 +57,14 @@ const openSportsQuestions = document.querySelector('#catagorySports');
 openSportsQuestions.addEventListener('click', sportsQuestions);
 
 function sportsQuestions(){
-    // Hide grid for catagories
-    let hideCatagories = document.querySelector('#main-catagory-grid');
-    hideCatagories.style.display='none';
 
-    // Show questions section
-    let openQuestionScreen = document.querySelector('#questions');
-    openQuestionScreen.style.display = 'flex';
-
-    // Add catagory title
+// Add catagory title
     let catagoryName = document.querySelector('#quiz-catagory');
     catagoryName.innerHTML = 'Smarticus:  Sport';
+
+    openQuestionScreen();
+    hideCatagoriesGrid();
 }
-
-/**
- * Creates sports questions
- */
-
-
 
 /**
  * Close the questions section and return to the catagory grid
@@ -62,11 +74,6 @@ const closeQuestions = document.querySelector('#close');
 closeQuestions.addEventListener('click', closeQuestionSection);
 
 function closeQuestionSection(){
-    // Show catagories grid 
-    let showCatagories = document.querySelector('#main-catagory-grid');
-    showCatagories.style.display = 'grid';
-
-    // Close questions sction
-    let closeQuestionSection = document.querySelector('#questions');
-    closeQuestionSection.style.display = 'none';
+    showCatagoriesGrid()
+    closeQuestionScreen()
 }
