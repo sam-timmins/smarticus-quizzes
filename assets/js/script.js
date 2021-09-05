ca83e288eeb308cb9a5476bff844927bd0844f24
sam-timmins <sam.timmins1@gmail.com>
2021-09-05 14:42:17 +0000

    Add style to #developer-name in style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,6 @@ const questionsScreenRef = document.querySelector('#questions');
 const insertCategoryNameRef = document.querySelector('#quiz-category');
 const closeQuestionsRef = document.querySelector('#close-questions');
 const questionRef = document.querySelector('#question');
-
 const categorySelectionRef = document.querySelectorAll('.category');
 const scoreRef = document.querySelector('#score');
 const questionNumberRef = document.querySelector('#question-number');
